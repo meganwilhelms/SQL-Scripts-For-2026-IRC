ab93560153f98843eaa17013edd7ec248713d53b
meganwilhelms <wilhelms.megan@gmail.com>
2026-04-28 10:48:25 -0500

    Update and rename Introduction Script to Introduction Script.sql

--- Introduction Script.sql
+++ Introduction Script.sql
@@ -1,6 +1,6 @@
 --Result set is a table of the first ten rows and all columns of the candidacy_v table
 SELECT Top 10 * 
-FROM TmsEPrd.dbo.candidacy_v
+FROM TmsEPrd.dbo.candidacy
 
 --Result set is a table with all columns of the candidacy_type_def table
 SELECT *
@@ -8,17 +8,17 @@ FROM TmsEPrd.dbo.candidacy_type_def
 
 --Result set is a table of the first ten rows and all columns from both of the joined tables
 SELECT Top 10 *
-FROM TmsEPrd.dbo.candidacy_v cv
-		LEFT JOIN TmsEPrd.dbo.CANDIDACY_TYPE_DEF ctd ON cv.CANDIDACY_TYPE=ctd.CANDIDACY_TYPE
+FROM TmsEPrd.dbo.candidacy c
+		LEFT JOIN TmsEPrd.dbo.CANDIDACY_TYPE_DEF ctd ON c.CANDIDACY_TYPE=ctd.CANDIDACY_TYPE
 
 --Result set is a table of the first ten rows and all columns from both of the joined tables, but only where the rows are 2025 and 30 (fall)
 SELECT Top 10 *
-FROM TmsEPrd.dbo.candidacy_v cv
-	LEFT JOIN TmsEPrd.dbo.CANDIDACY_TYPE_DEF ctd ON cv.CANDIDACY_TYPE=ctd.CANDIDACY_TYPE
-WHERE cv.yr_cde = 2025 AND cv.trm_cde = '30'
+FROM TmsEPrd.dbo.candidacy_v c
+	LEFT JOIN TmsEPrd.dbo.CANDIDACY_TYPE_DEF ctd ON c.CANDIDACY_TYPE=ctd.CANDIDACY_TYPE
+WHERE c.yr_cde = 2025 AND c.trm_cde = '30'
 
 --Result set is a table of the specified columns from the joined tables, where the rows are for year 2025 and term 30 (fall)
-SELECT cv.ID_NUM, cv.YR_CDE, cv.TRM_CDE, cv.CANDIDACY_TYPE, ctd.CANDIDACY_TYP_DESC
-FROM TmsEPrd.dbo.candidacy_v cv
-	LEFT JOIN TmsEPrd.dbo.CANDIDACY_TYPE_DEF ctd ON cv.CANDIDACY_TYPE=ctd.CANDIDACY_TYPE
-WHERE cv.yr_cde = 2025 AND cv.trm_cde = '30'
+SELECT c.ID_NUM, c.YR_CDE, c.TRM_CDE, c.CANDIDACY_TYPE, ctd.CANDIDACY_TYP_DESC
+FROM TmsEPrd.dbo.candidacy_v c
+	LEFT JOIN TmsEPrd.dbo.CANDIDACY_TYPE_DEF ctd ON c.CANDIDACY_TYPE=ctd.CANDIDACY_TYPE
+WHERE c.yr_cde = 2025 AND c.trm_cde = '30'
